44d0191c409ba0cd758f7d9e35614f9b8e8c51d1
Samasuarez <paknais_01@hotmail.com>
2023-04-16 23:04:27 -0300

    base de datos mongo

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const {dbAdmin, dbPassword,dbHost } = require('../src/config/db.config')
+// const {dbAdmin, dbPassword,dbHost } = require('../src/config/db.config')s
 const mongoConect = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${dbAdmin}:${dbPassword}@${dbHost}/?retryWrites=true&w=majority`
+      `mongodb+srv://paknais01:admin@cluster0.u2zhjog.mongodb.net/?retryWrites=true&w=majority`
     );
     console.log("db conected");
   } catch (error) {
@@ -11,3 +11,4 @@ const mongoConect = async () => {
   }
 };
 module.exports = mongoConect;
+// mongodb+srv://paknais01:<password>@cluster0.u2zhjog.mongodb.net/?retryWrites=true&w=majority
